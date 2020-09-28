6b0b4fb76cc07a6579d93590d6412a3e8e145d61
Mitchell Rodio <56127734+MitchellRodio@users.noreply.github.com>
2020-09-28 10:05:57 -0600

    Update Java Test

--- admissions.java
+++ admissions.java
@@ -18,7 +18,24 @@ public class Admissions {
         System.out.println("Enter your SAT Score: ");
 
         String classAwards = awards.nextLine();  
-        System.out.println("SAT: " + satScore "Class Rank: " + rankClass "Class award: " + awards );  
+
+          Scanner  location = new Scanner(System.in);  
+        System.out.println("Enter your State: ");
+
+        String stateLocation = awards.nextLine();
+        
+        if (stateLocation == "Florida") {
+            System. exit(0)
+        }
+// STATE LOCATION ^
+
+//         System.out.println("SAT: " + satScore "Class Rank: " + rankClass "Class award: " + awards );  <- TESTING
+        
+        if (scoreSAT >= 1200) && (classRank >= 85) && (classAwards >= 3) {
+            System.out.println("You can be accepted!)
+        } else {
+            System.out.println("You cannot be accepted!)
+        }
   
 
     }
